b22d480019b192233b4568f52439d376116c777a
KaizDA <54418108+Deokti@users.noreply.github.com>
2019-10-19 21:04:31 +0300

    Add files via upload

---
+++ GRANIT/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    //создаём функцию и даём ей название
+   function appearanceSquare(item) {
+       //обращение к функации и перебирание с помощью each
+       $(item).each(function(i) {
+           //при нажатии происзодится функция
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            //обращается к классу и при нажатии даём класс активе
+            $('.edward-list__link-color').eq(i).toggleClass('edward-list__link-color_active');
+            $('.square').eq(i).toggleClass('square_active');
+        });
+       });
+   }
+
+   appearanceSquare('.edward-list__link-color');
+   appearanceSquare('.square__close');
+});
+
+
