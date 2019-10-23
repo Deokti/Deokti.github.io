b799c4e22fb5b071d441b56c3592d342b0580b1b
Deokti <reilgelern@mail.ru>
2019-10-23 18:04:47 +0300

    Доработано

--- Eddie's-Car/js/script.js
+++ Eddie's-Car/js/script.js
@@ -1,1 +1,18 @@
-$(document).ready(function(){function appearanceSquare(item){$(item).each(function(i){$(this).on('click',function(e){e.preventDefault();$('.edward-list__link-color').eq(i).toggleClass('edward-list__link-color_active');$('.square').eq(i).toggleClass('square_active')})})}appearanceSquare('.edward-list__link-color');appearanceSquare('.square__close')});
+$(document).ready(function() {
+    //создаём функцию и даём ей название
+   function appearanceSquare(item) {
+       //обращение к функации и перебирание с помощью each
+       $(item).each(function(i) {
+           //при нажатии происзодится функция
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            //обращается к классу и при нажатии даём класс активе
+            $('.edward-list__link-color').eq(i).toggleClass('edward-list__link-color_active');
+            $('.square').eq(i).toggleClass('square_active');
+        });
+       });
+   }
+
+   appearanceSquare('.edward-list__link-color');
+   appearanceSquare('.square__close');
+});
