a17ad38136279bcb960ab091b492d775acecb1e2
Deokti <reilgelern@mail.ru>
2019-11-20 04:54:56 +0300

    Проблема со слайдером решена, но вероятно не совсем правильно

--- ADVOGRAND/js/script.js
+++ ADVOGRAND/js/script.js
@@ -1,20 +1,110 @@
 $(window).load(function () {
 
-    //табы на секции comment
+    //-----табы на секции comment-----
     $('ul.comment__tabs').on('click', 'li:not(.comment__tab_active)', function () {
         $(this)
             .addClass('comment__tab_active').siblings().removeClass('comment__tab_active')
             .closest('div.comment__block').find('div.comment__inner').removeClass('comment__inner_active').eq($(this).index()).addClass('comment__inner_active');
     });
+    //-----/табы на секции comment-----
 
-    //слайдер на секции comment
-    $('.comment__inner').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
+    //-----СЛАЙДЕР НА СЕКЦИИ comment-----
 
-        prevArrow: '<button type="button" class="arrow__left arrow__left_comment"><img src="img/left-arrow.svg" alt="стрелка влево" class="hover-arrow__left"></button>',
-        nextArrow: '<button type="button" class="arrow__right arrow__right_comment"><img src="img/right-arrow.svg" alt="стрелка вправо" class="hover-arrow__right"></button>',
-    });
+    //первый 
+    const slider = tns({
+        container: '.comment__inner',
+        items: 3,
+        slideBy: 'page',
+        speed: 900,
+        nav: false,
+        controls: false,
+        });
+        document.querySelector('.arrow__left').addEventListener('click', function() {
+        slider.goTo('prev');
+        });
+        document.querySelector('.arrow__right').addEventListener('click', function() {
+        slider.goTo('next');
+        });
 
+    //второй 
+    const sliderTwo = tns({
+        container: '.comment__inner_two',
+        items: 3,
+        slideBy: 'page',
+        speed: 900,
+        nav: false,
+        controls: false,
+        });
+        document.querySelector('.arrow__left').addEventListener('click', function() {
+            sliderTwo.goTo('prev');
+        });
+        document.querySelector('.arrow__right').addEventListener('click', function() {
+            sliderTwo.goTo('next');
+        });
+
+    //третий 
+    const sliderThree = tns({
+        container: '.comment__inner_three',
+        items: 3,
+        slideBy: 'page',
+        speed: 900,
+        nav: false,
+        controls: false,
+        });
+        document.querySelector('.arrow__left').addEventListener('click', function() {
+            sliderThree.goTo('prev');
+        });
+        document.querySelector('.arrow__right').addEventListener('click', function() {
+            sliderThree.goTo('next');
+        });
+
+    //четвёртый
+    const sliderFour = tns({
+        container: '.comment__inner_four',
+        items: 3,
+        slideBy: 'page',
+        speed: 900,
+        nav: false,
+        controls: false,
+        });
+        document.querySelector('.arrow__left').addEventListener('click', function() {
+            sliderFour.goTo('prev');
+        });
+        document.querySelector('.arrow__right').addEventListener('click', function() {
+            sliderFour.goTo('next');
+        });
+
+    //пятый
+    const sliderFive = tns({
+        container: '.comment__inner_five',
+        items: 3,
+        slideBy: 'page',
+        speed: 900,
+        nav: false,
+        controls: false,
+        });
+        document.querySelector('.arrow__left').addEventListener('click', function() {
+            sliderFive.goTo('prev');
+        });
+        document.querySelector('.arrow__right').addEventListener('click', function() {
+            sliderFive.goTo('next');
+        });
+
+    //шестой 
+    const sliderSix = tns({
+        container: '.comment__inner_six',
+        items: 3,
+        slideBy: 'page',
+        speed: 900,
+        nav: false,
+        controls: false,
+        });
+        document.querySelector('.arrow__left').addEventListener('click', function() {
+            sliderSix.goTo('prev');
+        });
+        document.querySelector('.arrow__right').addEventListener('click', function() {
+            sliderSix.goTo('next');
+        });
+
+    //------/СЛАЙДЕР НА СЕКЦИИ comment------
 });
