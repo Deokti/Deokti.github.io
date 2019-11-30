ee4a7284c1b3b2185625eec43918ff536fbc459f
Deokti <reilgelern@mail.ru>
2019-11-30 23:38:18 +0300

    Сделано несколько секций.

--- LOGOTYPE/js/script.js
+++ LOGOTYPE/js/script.js
@@ -1,4 +1,5 @@
 $(window).load(function() {
+    //----------слайдер в promo
     $('.promo').vegas({
         slides: [
             { src: 'img/promo/one-bg.jpg' },
@@ -6,6 +7,9 @@ $(window).load(function() {
         ],
         autoplay: false,
         cover: true,
+        transitionDuration: 700,        
+        preloadImage: true,
+        transition: 'fade'
     });
     
     //перелистывает вперёд
@@ -17,5 +21,52 @@ $(window).load(function() {
     $('[data-modal=previous]').on('click', function () {
         $('.promo').vegas('previous');
     });
+    //----------/.слайдер в promo
+
+
+    //---------Анимация изменения стрелочки в секции conlustate
+
+
+
+    //при нажатии добавляется модификатор на consultate-right
+    $('.consultate-theme__select_checked').on('click', function(e) {
+        e.preventDefault();
+        $('.consultate-theme').toggleClass('consultate-theme_checked');
+    })
+   
+    //отлавливаем нахождение (нажатие на кнопку) мыши в документе
+    $(document).mouseup(function (e){ 
+        e.preventDefault();
+        //обращение к нужному классу
+        let checked = $('.consultate-theme'); 
+        //если клик случился не по элементу, указанному выше (родителю)
+        if (!checked.is(e.target) 
+        // и не по элементам, которые ниже (дочерние)
+            && checked.has(e.target).length === 0) { 
+                //то с удаляем класс с модицикатором
+                $('.consultate-theme').removeClass('consultate-theme_checked')
+        }
+    });
+    //---------/.Анимация изменения стрелочки в секции conlustate
+
+
+    //---------Слайдер в секции Новости Компании (EVENT-COMPANY)
+    let slider = tns({
+        container: '.event-company__left-slider',
+        items: 1,
+        slideBy: 'page',
+        autoplay: false,
+        speed: 700,
+        nav: false,
+        controls: false,
+      });
+      document.querySelector('.arrow-left_event-company').onclick = function () {
+        slider.goTo('prev');
+      };
+      document.querySelector('.arrow-right_event-company').onclick = function () {
+        slider.goTo('next');
+      };
+      //---------/.Слайдер в секции Новости Компании (EVENT-COMPANY)
+
 });
 
