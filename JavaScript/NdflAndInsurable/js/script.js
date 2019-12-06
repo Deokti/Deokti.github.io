19e896e40f99bfdf031a75916170c4b3011ac5a0
Deokti <reilgelern@mail.ru>
2019-12-06 04:41:33 +0300

    Положил не те файлы.

--- JavaScript/NdflAndInsurable/js/script.js
+++ JavaScript/NdflAndInsurable/js/script.js
@@ -1,25 +1,40 @@
-document.querySelector('.task-one__button').onclick = taskButtonOne; //обращаемся к кнопке, которая и производит расчитывание
+document.querySelector('.button').addEventListener('click', function(e) {
+   let amountSalary, displayNdfl, fundFss, fundPension, fundFoms, traumatism, result, taskOne, taskTwo, taskThree, tastFour, tastFive, taskSix;
+
+   //ввод колличества зарплаты. Она и берётся за основу вычетов
+   amountSalary = document.querySelector('.enter-salary').value;
+
+   enterTraumatism = document.querySelector('.fund-traumatism').value;
+
+   //какой НДФЛ на данный момент. Сейчас это 13%
+   displayNdfl = 0.13;
+
+   //просчитывает НДФЛ и следующая задача отнимает от введённой зарплаты этот НДФЛ
+   taskOne = Math.floor(amountSalary * displayNdfl);
+   taskTwo = amountSalary - taskOne;
+
+   //фонд социального страхования 2,9%
+   fundFss = 0.029 //получается 2,9 
+   taskThree = amountSalary * fundFss; //taskTwo / 100 * fundFss более сложный пример. 
 
 
-function taskButtonOne() {
-    let grossEarnings, vacation, calculate, base, lars, track;
-    
-    grossEarnings = document.querySelector('.task-one__total-earnings').value; //первое число, общий зароботок за 12 месяцев
-    parseInt(grossEarnings)
+   //Фонд пенсионного страхования, на данный момент 22%
+   fundPension = 0.22;
+   tastFour = amountSalary * fundPension;
+   
+   //Фонд медицинского страхования, на данный момент 5,1%
+   fundFoms = 0.051;
+   tastFive = amountSalary * fundFoms;
 
-    vacation = document.querySelector('.task-one__vacation').value; //второе значение, на сколько дней человек уходит в отпуск
-    parseInt(vacation)
-
-    base = (12 * 29.3);
-    
-    calculate = grossEarnings / base * vacation;
-
-    lars = calculate * 0.13;
-
-    track = calculate - lars;
-
-    document.querySelector('.task-one__number-holidays_span').innerHTML = calculate;
-    document.querySelector('.task-one__number-ndfl_span').innerHTML = lars;
-    document.querySelector('.task-one__amount-hand_span').innerHTML = track;
-
-}
+   //травматизм (умножает зарплату на полученную сумму в value, которая делится на 100 для получения нужного процента)
+   taskSix = amountSalary * (enterTraumatism / 100);
+   
+   document.querySelector('.salary-deductions_span').innerHTML = amountSalary;
+   document.querySelector('.display-ndfl_span').innerHTML = taskOne;
+   document.querySelector('.deduction-ndfl_span').innerHTML = taskTwo;
+   document.querySelector('.fund-fss_span').innerHTML = taskThree;
+   document.querySelector('.fund-pension_span').innerHTML = tastFour;
+   document.querySelector('.fund-foms_span').innerHTML = tastFive;
+   document.querySelector('.fund-trauma_span').innerHTML = taskSix;
+});
+ 
