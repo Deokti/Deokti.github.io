9cc23ee8b69e8f6d1629ce5d72acbc89017d9a1e
Deokti <reilgelern@mail.ru>
2020-02-25 19:07:08 +0300

    Убрал появление гамбургера при скролле. Сделал так, чтобы он виднелся изначально.

--- WebDispute/Lada/src/js/hamburger.js
+++ WebDispute/Lada/src/js/hamburger.js
@@ -21,19 +21,3 @@ mobuleMenu.addEventListener('click', event => {
         }
     } 
 });
-
-
-// Появление навигации при скоролле
-// более 800 пикселей
-const mainNavigations = document.querySelector('.navigations');
-window.addEventListener('scroll', () => {
-    let pageY = window.pageYOffset
-
-    if (pageY >= 700) {
-        hamburder.classList.add('hamburder_show');
-        mainNavigations.classList.add('navigations_show');
-    } else {
-        hamburder.classList.remove('hamburder_show');
-        mainNavigations.classList.remove('navigations_show');
-    }
-});
--- WebDispute/Lada/src/js/progress-bar.js
+++ WebDispute/Lada/src/js/progress-bar.js
@@ -1,60 +1,36 @@
-let progressBar = document.querySelector('.progress-bar .progress-bar__list');
+let progressBarOne = document.querySelector('.progress-bar .progress-bar__list');
+
+function progressBar(pageY, less, more, element, addClass) {
+    if (pageY >= less && pageY <= more) {
+        element.classList.add(addClass);
+    } else {
+        element.classList.remove(addClass);
+    }
+}
+    
 window.addEventListener('scroll', () => {
     let pageY = window.pageYOffset;
     let pageWidth = screen.width;
-    console.log(pageY);
 
     // Если скролл меньше 770, то добавляется класс Header
-    if (pageY >= 0 && pageY < 491) {
-        progressBar.classList.add('progress-bar__list_header');
-    } else {
-        progressBar.classList.remove('progress-bar__list_header');
-    }
+    progressBar(pageY, 0, 470, progressBarOne, 'progress-bar__list_header');
 
     // Если скролл меньше 1517, то добавляется класс About
-    if (pageY > 491 && pageY < 1263) {
-        progressBar.classList.add('progress-bar__list_about');
-    }
-    else {
-        progressBar.classList.remove('progress-bar__list_about');
-    }
+    progressBar(pageY, 491, 1262, progressBarOne, 'progress-bar__list_about');
 
-     // Если скролл меньше 2000, то добавляется класс Gallery
-    if (pageY > 1263 && pageY < 2053) {
-        progressBar.classList.add('progress-bar__list_gallery');
-    } else {
-        progressBar.classList.remove('progress-bar__list_gallery');
-    }
+    // Если скролл меньше 2000, то добавляется класс Gallery
+    progressBar(pageY, 1263, 2052, progressBarOne, 'progress-bar__list_gallery');
 
     // Если скролл меньше 2246, то добавляется класс Models
-    if (pageY > 2053 && pageY < 2842) {
-        progressBar.classList.add('progress-bar__list_models');
-    } else {
-        progressBar.classList.remove('progress-bar__list_models');
-    }
+    progressBar(pageY, 2053, 2841, progressBarOne, 'progress-bar__list_models');
 
-    // Если скролл больше 3018, то добавляется класс Contacts
-    if (pageY > 2842) {
-        progressBar.classList.add('progress-bar__list_contacts');
-    } else {
-        progressBar.classList.remove('progress-bar__list_contacts');
-    }
+    // Если скролл больше 2842, добавляется класс Contacts
+    progressBar(pageY, 2842, 3189, progressBarOne, 'progress-bar__list_contacts');
 
 
+    // Если размер экрана больше 1800, то сработает это
     if (pageWidth > 1800) {
-
-        // Если скролл меньше 2246, то добавляется класс Models
-        if (pageY > 2053 && pageY < 2500) {
-            progressBar.classList.add('progress-bar__list_models');
-        } else {
-            progressBar.classList.remove('progress-bar__list_models');
-        }
-
-        // Если скролл больше 3018, то добавляется класс Contacts
-        if (pageY > 2500) {
-            progressBar.classList.add('progress-bar__list_contacts');
-        } else {
-            progressBar.classList.remove('progress-bar__list_contacts');
-        }
+        progressBar(pageY, 2053, 2500, progressBarOne, 'progress-bar__list_models');
+        progressBar(pageY, 2500, 3189, progressBarOne, 'progress-bar__list_contacts');
     }
 });
