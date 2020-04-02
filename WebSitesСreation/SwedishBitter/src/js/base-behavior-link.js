527748bbe2aae20b47739370ee81f05a32daa0a7
KaizDA <55704606+KaizDA@users.noreply.github.com>
2020-04-02 18:37:56 +0300

    Удалил ненужное.

--- WebSitesСreation/SwedishBitter/src/js/base-behavior-link.js
+++
@@ -1,7 +0,0 @@
-// Отключаем базовое поведение всех ссылок 
-let allLinks = document.querySelectorAll('.prevent-default');
-allLinks.forEach(event => {
-    event.addEventListener('click', event => {
-        event.preventDefault();
-    })
-});
