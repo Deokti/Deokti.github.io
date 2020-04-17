c67c5e7b8abb8042e38e29b55df3e799db0b59d0
KaizDA <reilgelern@mail.ru>
2020-04-17 18:22:41 +0300

    Пропустил шестую секцию (галерею), сделал и заадаптировал седьмую.

--- WebSitesСreation/ShapeUp/src/js/slider-gallery.js
+++ WebSitesСreation/ShapeUp/src/js/slider-gallery.js
@@ -1,8 +1,8 @@
-const slider = tns({
-    container: '#slider-gallery',
-    items: 2,
-    swipeAngle: false,
-    speed: 400,
-    nav: false,
-    edgePadding: 90,
-});
+// const slider = tns({
+//     container: '#slider-gallery',
+//     items: 2,
+//     swipeAngle: false,
+//     speed: 400,
+//     nav: false,
+//     edgePadding: 90,
+// });
