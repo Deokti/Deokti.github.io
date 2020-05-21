4599d67f9e5c57e16649ffaa89b0347d9f3dfacb
KaizDA <reilgelern@mail.ru>
2020-05-21 23:26:52 +0300

    123

--- WebSitesTrening/Creative/src/js/scripts/nav-mobile.js
+++
@@ -1,9 +0,0 @@
-const mobileMenu = (menuSelector, addClass) => {
-    const menu = document.querySelector(menuSelector);
-
-    if (!menu.classList.contains(addClass)) {
-        menu.classList.add(addClass);
-    } else menu.classList.remove(addClass);
-};
-
-export default mobileMenu;
