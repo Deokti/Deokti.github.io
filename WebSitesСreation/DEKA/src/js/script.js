6c3492570816b1bb67dfbfc7a2604877f41e29b6
Deokti <reilgelern@mail.ru>
2020-11-18 19:43:40 +0300

    ...

--- WebSitesСreation/DEKA/src/js/script.js
+++ WebSitesСreation/DEKA/src/js/script.js
@@ -1,5 +1,7 @@
 import behaviorHamburgerOnClick from './scripts/hamburger';
+import feedbackSlider from './scripts/feedback-slider';
 
 document.addEventListener('DOMContentLoaded', () => {
   behaviorHamburgerOnClick();
+  feedbackSlider();
 });
