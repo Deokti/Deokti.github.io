7baec774b0cf8cfbcf699c8996c42582eb22ee14
Deokti <reilgelern@mail.ru>
2020-12-10 14:14:16 +0300

    ...

--- WebSitesСreation/Dismantling/dist/js/scripts/popup-status-control.js
+++ WebSitesСreation/Dismantling/dist/js/scripts/popup-status-control.js
@@ -2,11 +2,21 @@
 // 1. Селектор самого popup который нужно открыть
 // 2. Селекто закрытия popup. В нашем случае он убирает активный класс
 // 3. Триггер-селектор, то есть кнопка на которую нажимается для открытия
-function popupStatusControl(selector, closeSelector, triggerSelector) {
+function popupStatusControl(selector, closeSelector, triggerSelector, changeSericeTitle = false) {
   const popup = document.querySelector(selector);
   const closePopup = document.querySelector(closeSelector);
   const body = document.querySelector('body');
   const trigger = document.querySelectorAll(triggerSelector);
+
+  const changePopupTitleInService = (event) => {
+    const stateTitle = 'УЗНАТЬ СТОИМОСТЬ ДЕМОНТАЖА';
+
+    const curentServiceItem = event.path[4];
+    const getServiceTitle = curentServiceItem.querySelector('.service__item-title').textContent.trim();
+    const skipFirstWord = (getServiceTitle.split(' ').slice(1).join(' ')).trim();
+    const popupTitle = popup.querySelector('.popup__title');
+    popupTitle.textContent = `${stateTitle} ${skipFirstWord.toUpperCase()}`;
+  };
 
   const close = () => {
     popup.classList.remove('active');
@@ -24,7 +34,10 @@ function popupStatusControl(selector, closeSelector, triggerSelector) {
     document.addEventListener('keyup', closePopupEvent);
   };
 
-  const open = () => {
+  const open = (event) => {
+    if (changeSericeTitle) {
+      changePopupTitleInService(event);
+    }
     popup.classList.add('active');
     body.style.overflow = 'hidden';
     closePopupESC();
@@ -36,5 +49,5 @@ function popupStatusControl(selector, closeSelector, triggerSelector) {
 popupStatusControl('.popup-call', '.popup-call .popup__close', 'button[data-event=request-call]');
 popupStatusControl('.popup-file', '.popup-file .popup__close', 'button[data-event=attach-file]');
 popupStatusControl('.popup-consultate', '.popup-consultate .popup__close', 'button[data-event=consultate]');
-popupStatusControl('.popup-costing', '.popup-costing .popup__close', 'button[data-event=get-calculation');
+popupStatusControl('.popup-costing', '.popup-costing .popup__close', 'button[data-event=get-calculation', true);
 popupStatusControl('.privacy-policy', '.privacy-policy__close', '.footer__privacy-policy');
--- WebSitesСreation/Dismantling/src/js/scripts/service-items.js
+++ WebSitesСreation/Dismantling/src/js/scripts/service-items.js
@@ -3,22 +3,26 @@
   const serviceItem = serviceList.querySelectorAll('.service__item');
 
   const ifItem = (event) => event.target.classList.contains('service__item');
+  const notActiveItem = (event) => !event.target.classList.contains('service__item--active')
   const stateClass = (event, action) => event.target.classList[action]('service__item--active');
   const removeClass = () => serviceItem.forEach(item => item.classList.remove('service__item--active'));
 
   const eventServiceItem = (event) => {
-    if (ifItem(event) && !event.target.classList.contains('service__item--active')) {
+    if (ifItem(event) && notActiveItem(event)) {
       removeClass();
       stateClass(event, 'add');
+      document.addEventListener('click', onCloseItemInfo);
       return false;
     }
     stateClass(event, 'remove');
   }
 
   const onCloseItemInfo = (event) => {
-    if (!event.path.includes(serviceList)) removeClass();
+    if (!event.path.includes(serviceList)) {
+      removeClass();
+      document.removeEventListener('click', onCloseItemInfo);
+    };
   }
 
   serviceList.addEventListener('click', eventServiceItem);
-  document.addEventListener('click', onCloseItemInfo)
 }());
